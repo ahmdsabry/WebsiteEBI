ece335f6b067e37d1fa6b55a9063f8100518e47d
AHMED SABRY <105075189+ahmdsabry@users.noreply.github.com>
2024-01-18 05:06:54 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,171 +1,167 @@
-var users = JSON.parse(localStorage.getItem('users')) || [];
+ function openpop() {
+     var pop = document.getElementById("popup");
 
-function openpop() {
-    var pop = document.getElementById("popup");
+     pop.classList.add("popup-open");
+ }
 
-    pop.classList.add("popup-open");
-}
+ function openpop2() {
+     var pop = document.getElementById("popup2");
 
-function openpop2() {
-    var pop = document.getElementById("popup2");
+     pop.classList.add("popup-open2");
+ }
 
-    pop.classList.add("popup-open2");
-}
+ function openpop4() {
+     var pop = document.getElementById("popup4");
 
-function openpop4() {
-    var pop = document.getElementById("popup4");
+     pop.classList.add("popup-open2");
+ }
 
-    pop.classList.add("popup-open2");
-}
+ function openpop3() {
+     var pop = document.getElementById("popup3");
 
-function openpop3() {
-    var pop = document.getElementById("popup3");
+     pop.classList.add("popup-open3");
+ }
 
-    pop.classList.add("popup-open3");
-}
+ function closepop() {
+     var pop = document.getElementById("popup");
 
-function closepop() {
-    var pop = document.getElementById("popup");
+     pop.classList.remove("popup-open");
+ }
 
-    pop.classList.remove("popup-open");
-}
+ function closepop4() {
+     var pop = document.getElementById("popup4");
 
-function closepop4() {
-    var pop = document.getElementById("popup4");
+     pop.classList.remove("popup-open2");
+ }
 
-    pop.classList.remove("popup-open2");
-}
 
 
 
 
 
 
+ function registerUser() {
+     var email = document.getElementById('email').value;
+     var password = document.getElementById('password').value;
+     var Password2 = document.getElementById('confirmPassword').value;
+     var phone = document.getElementById('phone').value;
 
-function registerUser() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    var Password2 = document.getElementById('confirmPassword').value;
-    var phone = document.getElementById('phone').value;
-    // var selectedGenderElement = document.querySelector('input[name="gender"]:checked');
 
 
+     var gender;
+     if (document.getElementById("male").checked) {
+         gender = document.getElementById("male").value;
+     } else {
 
-    var gender;
-    if (document.getElementById("male").checked) {
-        gender = document.getElementById("male").value;
-    } else {
+         gender = document.getElementById("female").value;
 
-        gender = document.getElementById("female").value;
+     }
 
-    }
-    localStorage.setItem('gender', gender);
+     if (password !== Password2) {
+         openpop4();
+         return;
+     }
+     if (email.indexOf('@gmail') === -1) {
+         openpop2();
+         return;
 
+     } else {
+         var userData = {
+             email: email,
+             password: password,
+             confirmPassword: Password2,
+             phone: phone,
+             gender: gender
+         };
 
+         // Convert the JavaScript object to JSON
+         var jsonData = JSON.stringify(userData);
+         localStorage.setItem('userData', jsonData);
+         openpop();
 
-    localStorage.setItem('email', email);
-    localStorage.setItem('password', password);
-    localStorage.setItem('phone', phone);
 
-    if (password !== Password2) {
-        openpop4();
-        return;
-    }
-    if (email.indexOf('@gmail') === -1) {
-        openpop2();
-        return;
+     }
 
-    } else {
+ }
 
-        openpop();
 
 
-    }
-    /*
-    var userExists = users.some(function(egUser) {
-        return egUser.email === email;
-    });
 
-    if (userExists) {
-        alert("  email already exists");
-    } else {
-        var user = {
-            email: email,
-            password: password,
-            phone: phone,
-            gender: gender
-        };
 
-        users.push(user);
 
-        localStorage.setItem('users', JSON.stringify(users));
-*/
-}
+ function loginUser() {
 
 
+     var storedData = localStorage.getItem('userData');
+     if (storedData) {
+         var userData = JSON.parse(storedData);
 
+         var email = document.getElementById('email').value;
+         var password = document.getElementById('password').value;
+         if (email === userData.email && password === userData.password) {
 
+             localStorage.setItem('loggedInUser', JSON.stringify(userData));
 
+             var pop = document.getElementById("popup");
 
+             pop.classList.add("popup-open");
+         } else {
+             var pop = document.getElementById("popup5");
 
-function move() {
-    window.location.href = "../Login/login.html";
+             pop.classList.add("popup-open");
+         }
+     } else {
+         alert("Invalid email or password");
 
-}
+     }
+ }
 
 
 
 
-function closePopup() {
-    var popup = document.getElementById('popup3');
-    popup.style.display = 'none';
-}
 
-function closePopup() {
-    var popup = document.getElementById('popup2');
-    popup.style.display = 'none';
-}
 
 
+ function move() {
+     window.location.href = "../Login/login.html";
 
+ }
 
 
 
 
-var users = JSON.parse(localStorage.getItem('users')) || [];
+ function closePopup() {
+     var popup = document.getElementById('popup3');
+     popup.style.display = 'none';
+ }
 
-function loginUser() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+ function closePopup() {
+     var popup = document.getElementById('popup2');
+     popup.style.display = 'none';
 
-    var userEmail = localStorage.getItem("email");
-    var userpassword = localStorage.getItem("password");
 
+ }
 
-    /* var loggedInUser = users.find(function(user) {
-         return user.email === email && user.password === password;
-     });*/
+ function closepopup5() {
+     popup = document.getElementById('popup5');
+     popup.style.display = 'none';
+ }
 
-    /*   if (loggedInUser) {
-           var pop = document.getElementById("popup");
 
-           pop.classList.add("popup-open");
-       } else {
-           alert("Invalid email or password");
-       }*/
 
-    if (email == userEmail && password == userpassword) {
 
-        var pop = document.getElementById("popup");
 
-        pop.classList.add("popup-open");
-    } else {
-        alert("Invalid email or password");
 
-    }
-}
 
-function movetohome() {
-    window.location.href = "../home/index.html";
 
-}
+
+
+ function movetohome() {
+     window.location.href = "../home/index.html";
+
+ }
+
+ function movetohome() {
+     window.location.href = "../home/index.html";
+
+ }
